2e1c7e701a390e63b8f2a6e3ffc47c0cab23ec58
davinchi73 <dev107c.d133@gmail.com>
2023-11-17 14:04:26 -0600

    updates to home.html

--- javaScriptFiles/Home.js
+++ javaScriptFiles/Home.js
@@ -1,3 +1,32 @@
 function redirectToPage(page) {
     window.location.href = page;
 }
+
+// Functions to handle modals
+function openModal() {
+    document.getElementById('imageModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('imageModal').style.display = 'none';
+}
+
+function openImageModal(imageSrc) {
+    const displayedImage = document.getElementById('displayedImage');
+    displayedImage.src = imageSrc;
+    document.getElementById('individualImageModal').style.display = 'block';
+}
+
+function closeIndividualModal() {
+    document.getElementById('individualImageModal').style.display = 'none';
+}
+
+// Close modals on clicking outside the modal content
+window.onclick = function (event) {
+    const modal = document.getElementsByClassName('modal');
+    for (let i = 0; i < modal.length; i++) {
+        if (event.target === modal[i]) {
+            modal[i].style.display = 'none';
+        }
+    }
+};
