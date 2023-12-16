86c5521e88af8e1bd895798cf6c92b591b02e485
Stephanie <stgeber4789@gmail.com>
2023-12-16 14:11:46 -0600

    Stuff db created

--- javaScriptFiles/Storage.js
+++ javaScriptFiles/Storage.js
@@ -11,10 +11,12 @@
 // Create db instance
 let db;
 let db_Points;
+let db_Stuff;
 
 // Open/create databases
 const openRequest = window.indexedDB.open("tasks_db", 1);
 const openRequestPoints = window.indexedDB.open("points_db", 1);
+const openRequestStuff = window.indexedDB.open("stuff_db", 1);
 
 // Error/success handlers db
 openRequest.addEventListener("error", () => console.error("Database failed to open"), );
@@ -32,10 +34,20 @@ openRequestPoints.addEventListener("error", () => console.error("Points database
 openRequestPoints.addEventListener("success", () => {
   console.log("Points database opened successfully");
 
-  // Store open database object in db
+  // Store open database object
   db_Points = openRequestPoints.result;
 
   updatePointsLabel();
+
+});
+
+// Error/success handlers db_Stuff
+openRequestStuff.addEventListener('error', () => console.error("Stuff database failed to open"), );
+openRequestStuff.addEventListener('success', () => {
+  console.log("Stuff database opened successfully");
+
+  // Store open database object
+  db_Stuff = openRequestStuff.result;
 
 });
 
@@ -68,6 +80,18 @@ openRequestPoints.addEventListener("upgradeneeded", (ev) => {
   objectStorePoints.createIndex('points', 'points', {unique: false});
   
   console.log("Points database setup complete");
+
+});
+
+// Database (stuff) creation
+openRequestStuff.addEventListener("upgradeneeded", (event) => {
+  db_Stuff.event.target.result;
+  console.log("Stuff db grabbed");
+
+  const objectStoreStuff = db_Stuff.createObjectStore("stuff_db");
+
+  objectStoreStuff.createIndex("thing", "thing", {unique: false});
+  console.log("Stuff database setup complete");
 
 });
 
